3c52eb9650f448ec8a6c6e49a0756c6a124e726a
patrickfrey <patrickpfrey@yahoo.com>
2018-06-22 08:00:06 +0200

    added option --debug to module loader

--- tests/loader/testModuleLoader.cpp
+++ tests/loader/testModuleLoader.cpp
@@ -9,6 +9,7 @@
 #include "strus/lib/error.hpp"
 #include "strus/moduleLoaderInterface.hpp"
 #include "strus/errorBufferInterface.hpp"
+#include "strus/debugTraceInterface.hpp"
 #include "testModuleDirectory.hpp"
 #include "strus/base/local_ptr.hpp"
 #include <memory>
@@ -16,10 +17,20 @@
 #include <stdexcept>
 #include <iostream>
 #include <cstdio>
+#include <cstring>
+
+static void printUsage()
+{
+	std::cerr << "testModuleLoader [options] <modulename>" << std::endl;
+	std::cerr << "Options:" << std::endl;
+	std::cerr << "       -G|--debug <ID>    :enable debug for <ID>" << std::endl;
+	std::cerr << "       -h|--help          :print this usage" << std::endl;
+}
 
 int main( int argc, const char** argv)
 {
-	strus::local_ptr<strus::ErrorBufferInterface> errorbuf( strus::createErrorBuffer_standard( stderr, 1, NULL/*debug trace interface*/));
+	strus::DebugTraceInterface* dbgtrace = strus::createDebugTrace_standard( 2);
+	strus::local_ptr<strus::ErrorBufferInterface> errorbuf( strus::createErrorBuffer_standard( stderr, 1, dbgtrace));
 	if (!errorbuf.get())
 	{
 		std::cerr << "error creating error buffer" << std::endl;
@@ -34,7 +45,38 @@ int main( int argc, const char** argv)
 	std::cerr << "setting load module path to '" << STRUS_TEST_MODULE_DIRECTORY << "'" << std::endl;
 	modloader->addModulePath( STRUS_TEST_MODULE_DIRECTORY);
 
-	int ai = 1, ae = argc;
+	int argi = 1;
+	for (; argi < argc && argv[argi][0] == '-'; ++argi)
+	{
+		if (0==std::strcmp( argv[argi], "--debug") || 0==std::strcmp( argv[argi], "-G"))
+		{
+			if (!argv[++argi]) throw std::runtime_error( "missing argument for option --debug / -G");
+			if (!dbgtrace->enable( argv[argi])) throw std::runtime_error( "failed to enable debug");
+		}
+		else if (0==std::strcmp( argv[argi], "--help") || 0==std::strcmp( argv[argi], "-h"))
+		{
+			printUsage();
+			exit( 0);
+		}
+		else if (0==std::strcmp( argv[argi], "--"))
+		{
+			argi++;
+			break;
+		}
+		else
+		{
+			std::cerr << "Unknown option " << argv[argi] << std::endl;
+			printUsage();
+			exit( 1);
+		}
+	}
+	if (argi == argc)
+	{
+		std::cerr << "Too few arguments" << std::endl;
+		printUsage();
+		exit( 1);
+	}
+	int ai = argi, ae = argc;
 	for (; ai != ae; ++ai)
 	{
 		std::cerr << "loading module '" << argv[ai] << "'" << std::endl;
